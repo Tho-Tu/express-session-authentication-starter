d450ded95de80bec05471b0a4651fd9e2c2d80f5
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2024-04-29 14:56:47 +1000

    Authenticate users

--- app.js
+++ app.js
@@ -1,44 +1,65 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const session = require('express-session');
-var passport = require('passport');
-var crypto = require('crypto');
-var routes = require('./routes');
-const connection = require('./config/database');
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+var passport = require("passport");
+var crypto = require("crypto");
+var routes = require("./routes");
+const connection = require("./config/database");
 
 // Package documentation - https://www.npmjs.com/package/connect-mongo
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require("connect-mongo")(session);
 
 // Need to require the entire Passport config module so app.js knows about it
-require('./config/passport');
+require("./config/passport");
 
 /**
  * -------------- GENERAL SETUP ----------------
  */
 
 // Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
-require('dotenv').config();
+require("dotenv").config();
 
 // Create the Express application
 var app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 /**
  * -------------- SESSION SETUP ----------------
  */
 
+const sessionStore = new MongoStore({
+  mongooseConnection: connection,
+  collection: "session",
+});
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60min/1hr * 60sec/1min * 1000ms/1sec)
+    },
+  })
+);
 // TODO
 
 /**
  * -------------- PASSPORT AUTHENTICATION ----------------
  */
 
-app.use(passport.initialize());
+require("./config/passport");
+// app.use(passport.initialize()); This line is no longer required to include in current versions of Passport.
 app.use(passport.session());
 
+app.use((req, res, next) => {
+  console.log(req.session);
+  console.log(req.user);
+  next();
+});
 
 /**
  * -------------- ROUTES ----------------
@@ -47,6 +68,9 @@ app.use(passport.session());
 // Imports all of the routes from ./routes/index.js
 app.use(routes);
 
+app.use((err, req, res, next) => {
+  res.status(500).send(err);
+});
 
 /**
  * -------------- SERVER ----------------
